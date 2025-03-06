63bca6ad787dbc6691628a429fbb8a7e199cbed8
Orujhimaru <159823243+Orujhimaru@users.noreply.github.com>
2025-03-06 22:14:03 +0400

    design: practice bars changed

--- src/pages/CoursesPage/Courses.jsx
+++ src/pages/CoursesPage/Courses.jsx
@@ -71,6 +71,14 @@ const courseData = [
   },
 ];
 
+const formatTime = (seconds) => {
+  const minutes = Math.floor(seconds / 60);
+  const remainingSeconds = seconds % 60;
+  return `${minutes.toString().padStart(2, "0")}:${remainingSeconds
+    .toString()
+    .padStart(2, "0")} s`;
+};
+
 const Courses = () => {
   return (
     <div className="courses-page">
